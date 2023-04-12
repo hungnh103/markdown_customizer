40c32803b4d8b2e8631d069c77cf5e051def5f9e
Nguyễn Huy Hùng <nguyen.huy.hung-c@sun-asterisk.com>
2023-04-13 00:00:10 +0700

    feat: apply MUI tab

--- src/App.js
+++ src/App.js
@@ -1,8 +1,41 @@
+import { useState } from 'react'
+
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+import Box from '@mui/material/Box';
+
+import TabPanel from './lib/TabPanel'
+
+function a11yProps(index) {
+  return {
+    id: `simple-tab-${index}`,
+    'aria-controls': `simple-tabpanel-${index}`,
+  };
+}
+
 function App() {
+  const [value, setValue] = useState(0);
+
+  const handleChange = (_event, newValue) => {
+    setValue(newValue);
+  };
+
   return (
-    <div className="App">
-      hello
-    </div>
+    <Box sx={{ width: '100%' }}>
+      <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+        <Tabs value={value} onChange={handleChange}>
+          <Tab label="Table" {...a11yProps(0)} />
+          <Tab label="Image" {...a11yProps(1)} />
+        </Tabs>
+      </Box>
+
+      <TabPanel value={value} index={0}>
+        Item One
+      </TabPanel>
+      <TabPanel value={value} index={1}>
+        Item Two
+      </TabPanel>
+    </Box>
   );
 }
 
---
+++ src/lib/TabPanel.js
@@ -0,0 +1,31 @@
+import PropTypes from 'prop-types';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+
+function TabPanel(props) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box sx={{ p: 3 }}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
+}
+
+TabPanel.propTypes = {
+  children: PropTypes.node,
+  index: PropTypes.number.isRequired,
+  value: PropTypes.number.isRequired,
+};
+
+export default TabPanel
